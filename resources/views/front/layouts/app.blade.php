383a8c71747b0453dcdb4d7088cd1c061dfaa0fa
mezbah009 <hmezbahoffice@gmail.com>
2025-08-04 21:57:09 +0600

    navbar edit

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <meta name="keywords" content="">
 
     <!-- title -->
-    <title>Taxrio - Tax Advisor & Consulting HTML5 Template</title>
+    <title>TaxPilot - Tax Advisor & Consulting</title>
 
     <!-- favicon -->
     <link rel="icon" type="image/x-icon" href="assets/img/logo/favicon.png">
@@ -44,7 +44,7 @@
         <div class="main-navigation">
             <nav class="navbar navbar-expand-lg">
                 <div class="container position-relative">
-                    <a class="navbar-brand" href="index.html">
+                    <a class="navbar-brand" href="#">
                         <img src="{{ asset('front-assets/assets/img/logo/logo-light.png') }}" class="logo-display" alt="logo">
                         <img src="{{ asset('front-assets/assets/img/logo/logo.png') }}" class="logo-scrolled" alt="logo">
                     </a>
@@ -59,9 +59,10 @@
                     </div>
                     <div class="collapse navbar-collapse" id="main_nav">
                         <ul class="navbar-nav">
-                            <li class="nav-item dropdown">
+                            <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                            {{-- <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle active" href="index-3.html#" data-bs-toggle="dropdown">Home</a>
-                                {{-- <ul class="dropdown-menu fade-down">
+                                <ul class="dropdown-menu fade-down">
                                     <li><a class="dropdown-item" href="index.html">Home Page 01</a></li>
                                     <li><a class="dropdown-item" href="index-2.html">Home Page 02</a></li>
                                     <li><a class="dropdown-item" href="index-3.html">Home Page 03</a></li>
@@ -69,9 +70,9 @@
                                     <li><a class="dropdown-item" href="index-5.html">Home Page 05</a></li>
                                 </ul>
                             </li> --}}
-                            <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">About</a></li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="index-3.html#">Services</a>
+                                <a class="nav-link" href="#">Services</a>
                                 <ul class="dropdown-menu fade-down">
                                     <li><a class="dropdown-item" href="service.html">Services One</a></li>
                                     <li><a class="dropdown-item" href="service-2.html">Services Two</a></li>
@@ -79,57 +80,57 @@
                                 </ul>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="index-3.html#" data-bs-toggle="dropdown">Pages</a>
+                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Pages</a>
                                 <ul class="dropdown-menu fade-down">
-                                    <li><a class="dropdown-item" href="about.html">About Us</a></li>
-                                    <li><a class="dropdown-item" href="team.html">Our Team</a></li>
+                                    <li><a class="dropdown-item" href="#">About Us</a></li>
+                                    <li><a class="dropdown-item" href="#">Our Team</a></li>
                                     <li class="dropdown-submenu">
-                                        <a class="dropdown-item dropdown-toggle" href="index-3.html#">Authentication</a>
+                                        <a class="dropdown-item dropdown-toggle" href="#">Authentication</a>
                                         <ul class="dropdown-menu">
-                                            <li><a class="dropdown-item" href="login.html">Login</a></li>
-                                            <li><a class="dropdown-item" href="register.html">Register</a></li>
-                                            <li><a class="dropdown-item" href="forgot-password.html">Forgot Password</a>
+                                            <li><a class="dropdown-item" href="#">Login</a></li>
+                                            <li><a class="dropdown-item" href="#">Register</a></li>
+                                            <li><a class="dropdown-item" href="#">Forgot Password</a>
                                             </li>
                                         </ul>
                                     </li>
                                     <li class="dropdown-submenu">
-                                        <a class="dropdown-item dropdown-toggle" href="index-3.html#">Extra Pages</a>
+                                        <a class="dropdown-item dropdown-toggle" href="#">Extra Pages</a>
                                         <ul class="dropdown-menu">
-                                            <li><a class="dropdown-item" href="404.html">404 Error</a></li>
-                                            <li><a class="dropdown-item" href="coming-soon.html">Coming Soon</a></li>
-                                            <li><a class="dropdown-item" href="terms.html">Terms Of Service</a></li>
-                                            <li><a class="dropdown-item" href="privacy.html">Privacy Policy</a></li>
+                                            <li><a class="dropdown-item" href="#">404 Error</a></li>
+                                            <li><a class="dropdown-item" href="#">Coming Soon</a></li>
+                                            <li><a class="dropdown-item" href="#">Terms Of Service</a></li>
+                                            <li><a class="dropdown-item" href="#">Privacy Policy</a></li>
                                         </ul>
                                     </li>
-                                    <li><a class="dropdown-item" href="contact.html">Contact Us</a></li>
-                                    <li><a class="dropdown-item" href="gallery.html">Gallery</a></li>
-                                    <li><a class="dropdown-item" href="pricing.html">Pricing Plan</a></li>
-                                    <li><a class="dropdown-item" href="faq.html">Faq's</a></li>
-                                    <li><a class="dropdown-item" href="testimonial.html">Testimonials</a></li>
+                                    <li><a class="dropdown-item" href="#">Contact Us</a></li>
+                                    <li><a class="dropdown-item" href="#">Gallery</a></li>
+                                    <li><a class="dropdown-item" href="#">Pricing Plan</a></li>
+                                    <li><a class="dropdown-item" href="#">Faq's</a></li>
+                                    <li><a class="dropdown-item" href="#">Testimonials</a></li>
                                 </ul>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="index-3.html#" data-bs-toggle="dropdown">Case Study</a>
+                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Case Study</a>
                                 <ul class="dropdown-menu fade-down">
                                     <li><a class="dropdown-item" href="case-study.html">Case Study</a></li>
                                     <li><a class="dropdown-item" href="case-study-single.html">Case Study Single</a></li>
                                 </ul>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="index-3.html#" data-bs-toggle="dropdown">Blog</a>
+                                <a class="nav-link dropdown-toggle" href="l#" data-bs-toggle="dropdown">Blog</a>
                                 <ul class="dropdown-menu fade-down">
-                                    <li><a class="dropdown-item" href="blog.html">Blog</a></li>
-                                    <li><a class="dropdown-item" href="blog-single.html">Blog Single</a></li>
+                                    <li><a class="dropdown-item" href="#">Blog</a></li>
+                                    <li><a class="dropdown-item" href="#">Blog Single</a></li>
                                 </ul>
                             </li>
-                            <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
                         </ul>
                         <div class="nav-right">
                             <div class="search-btn">
                                 <button type="button" class="nav-right-link search-box-outer"><i class="far fa-search"></i></button>
                             </div>
                             <div class="nav-right-btn">
-                                <a href="https://taxpilot.opuserp.com/tax/register-personal" class="theme-btn">Get Started<i class="fas fa-arrow-right"></i></a>
+                                <a href="#" class="theme-btn">Get Started<i class="fas fa-arrow-right"></i></a>
                             </div>
                         </div>
                     </div>
